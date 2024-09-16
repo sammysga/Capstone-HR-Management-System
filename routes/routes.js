ca73159e3ac54ff15f73616c176284483d55e32a
Jushk <justinepicaze@gmail.com>
2024-09-16 22:04:58 +0800

    .

--- routes/routes.js
+++ routes/routes.js
@@ -16,7 +16,7 @@ router.get('/applicant_signup', applicantController.getPublicSignUp);
 router.post('/applicant_signup_submit', applicantController.handleRegisterPage);
 router.get('/login/staff', staffLoginController.getStaffLogin);
 router.post('/login/staff', staffLoginController.postStaffLogin);
-router.get('/home', applicantController.getApplicantHome);
+router.get('/about', applicantController.getAboutPage);
 
 
 // Protected routes (role-specific logic in respective controllers)
