19372a33b362809dd2fe6e037721e088749cbdf5
charissevaldez <charisseashantiv@gmail.com>
2024-09-26 14:48:59 +0800

    employee and line manager routes set (working)

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,7 +1,7 @@
 const employeeController = {
     getEmployeeDashboard: function(req, res) {
         if (req.session.user && req.session.user.userRole === 'Employee') {
-            res.render('employee/dashboard');
+            res.render('staffpages/employee_pages/employeedashboard');
         } else {
             req.flash('errors', { authError: 'Unauthorized. Employee access only.' });
             res.redirect('/login/staff');
