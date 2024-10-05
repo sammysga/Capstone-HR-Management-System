3db277010e17f007ba03c45cf1248e68db3710bc
sammysga <samantha.aniversario@gmail.com>
2024-10-05 22:10:25 +0800

    hr delete announcement

--- routes/routes.js
+++ routes/routes.js
@@ -43,7 +43,7 @@ router.get('/hr/managehome', hrController.getHRManageHome);
 router.get('/hr/addannouncement', hrController.getAddAnnouncement);
 router.post('/hr/addannouncement', hrController.postAddAnnouncement);
 router.get('hr/editannouncement/:id', hrController.getAddAnnouncement);
-router.delete('/hr/deleteannouncement/:id', hrController.deleteAnnouncement);
+router.delete('/hr/deleteannouncement/:announcementID', hrController.deleteAnnouncement);
 router.get('/hr/joboffers', hrController.getJobOffers);
 
 
