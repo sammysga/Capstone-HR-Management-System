eabccf5c2cccdf35a3145a949a3b763a79219ead
sammysga <samantha.aniversario@gmail.com>
2024-10-17 09:18:23 +0800

    get request new mrf form

--- controllers/lineManagerController.js
+++ controllers/lineManagerController.js
@@ -16,6 +16,15 @@ const lineManagerController = {
             res.redirect('/staff/login');
         }
     },
+
+    getRequestMRF: function(req, res) {
+        if (req.session.user && req.session.user.userRole === 'Line Manager') {
+            res.render('staffpages/linemanager_pages/request-mrf');
+        } else {
+            req.flash('errors', { authError: 'Unauthorized. Line Manager access only.' });
+            res.redirect('/staff/login');
+        }
+    }
 };
 
 module.exports = lineManagerController;
