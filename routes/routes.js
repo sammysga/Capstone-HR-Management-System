8d2a076c530f5e30fbe7940b0745cc16654e661c
jushk <justinepicaze@gmail.com>
2024-10-17 20:06:15 +0800

    modified employeeController and routes

--- routes/routes.js
+++ routes/routes.js
@@ -54,6 +54,7 @@ router.get('/hr/editjoboffers', hrController.getEditJobOffers);
 // Employee Routes
 router.get('/employee/dashboard', employeeController.getEmployeeDashboard);
 router.get('/employee/useracc', employeeController.getUserAccount);
+router.get('/employee/persinfocareerprog', employeeController.getPersInfoCareerProg);
 router.post('/employee/reset-password', employeeController.resetPassword);
 router.post('/employee/update-info', employeeController.updateUserInfo);
 
