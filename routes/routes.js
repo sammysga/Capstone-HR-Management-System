6b6efe50844aad791f034ef1adb9c696b63564df
jushk <justinepicaze@gmail.com>
2024-10-18 12:43:16 +0800

    added employeeoffboarding

--- routes/routes.js
+++ routes/routes.js
@@ -54,9 +54,11 @@ router.get('/hr/editjoboffers', hrController.getEditJobOffers);
 // Employee Routes
 router.get('/employee/dashboard', employeeController.getEmployeeDashboard);
 router.get('/employee/useracc', employeeController.getUserAccount);
+router.get('/employee/employeeoffboarding', employeeController.getEmployeeOffboarding);
 router.get('/employee/persinfocareerprog', employeeController.getPersInfoCareerProg);
 router.post('/employee/reset-password', employeeController.resetPassword);
 router.post('/employee/update-info', employeeController.updateUserInfo);
+
 
 // Lina Manager Routes
 router.get('/linemanager/dashboard', lineManagerController.getLineManagerDashboard);
