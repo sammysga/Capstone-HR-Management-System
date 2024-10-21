93c6abec948fe0e3949199bb722c96792b83d71e
Jushk <justinepicaze@gmail.com>
2024-10-21 11:01:41 +0800

    fixing employeeController

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -163,7 +163,7 @@ getPersInfoCareerProg: async function(req, res) {
         const { data: milestones, error: milestonesError } = await supabase
             .from('staffcareerprogression')
             .select('milestoneName, startDate, endDate')
-            .eq('staffId', staff[0]?.staffId)
+            .eq('staffId', staffId)
 
         // Check for errors
         if (userError || staffError || jobError || departmentError) {
