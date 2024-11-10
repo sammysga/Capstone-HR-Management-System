ad0a1cb6b0a45188de8678eb3320a022e6a5e520
Jushk <justinepicaze@gmail.com>
2024-11-10 22:45:33 +0800

    modified hreditjoboffers

--- routes/routes.js
+++ routes/routes.js
@@ -64,7 +64,8 @@ router.delete('/hr/deleteannouncement/:announcementID', hrController.deleteAnnou
 router.get('/hr/joboffers', hrController.getJobOffers);
 router.get('/hr/addjoboffer', hrController.getAddJobOffer);
 router.post('/hr/addjoboffer', hrController.postAddJobOffer);
-router.get('/hr/editjoboffers', hrController.getEditJobOffers);
+router.get('/hr/editjoboffers/:id', hrController.getEditJobOffers);
+router.post('/hr/editjoboffers/:id', hrController.updateJobOffer);
 router.get('/hr/records-performance-tracker', hrController.getRecordsPerformanceTracker);
 router.get('/hr/records-performance-tracker/:userId', hrController.getRecordsPerformanceTrackerByUserId);
 
