abf47db0ce5658e9f86599c000e0ed33b7b2c949
charissevaldez <charisseashantiv@gmail.com>
2024-11-15 07:55:47 +0800

    i hate supabase sa db pala may error

--- routes/routes.js
+++ routes/routes.js
@@ -132,6 +132,7 @@ router.get(
     lineManagerController.getUserProgressView
 );
 router.post('/linemanager/records-performance-tracker/:userId', lineManagerController.saveObjectiveSettings);
+router.post('/linemanager/records-performance-tracker/questionnaire/:userId', lineManagerController.saveQ1_360DegreeFeedback);
 
 module.exports = router; 
 
