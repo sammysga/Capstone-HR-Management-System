9ea7a747bfa652e221f6de198513896f4f7850c5
Jushk <justinepicaze@gmail.com>
2024-11-18 21:09:18 +0800

    fixed error

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -1,7 +1,7 @@
 const supabase = require('../public/config/supabaseClient');
 const bcrypt = require('bcrypt');
 const expressFileUpload = require('express-fileupload');
-const path = require('path');
+
 const { getJobDetails } = require('./applicantController');
 const fs = require('fs');
 const path = require('path');
