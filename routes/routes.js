ed5699f31ad68a53b39c1f916ce458d59ed904fa
Jushk <justinepicaze@gmail.com>
2024-11-25 16:01:15 +0800

    fixed routes

--- routes/routes.js
+++ routes/routes.js
@@ -69,8 +69,8 @@ router.delete('/hr/deleteannouncement/:announcementID', hrController.deleteAnnou
 router.get('/hr/joboffers', hrController.getJobOffers);
 router.get('/hr/addjoboffer', hrController.getAddJobOffer);
 router.post('/hr/addjoboffer', hrController.postAddJobOffer);
-router.get('/hr/editjoboffers/:jobId', hrController.getEditJobOffers);
-router.post('/hr/editjoboffers/:id', hrController.updateJobOffer);
+router.get('/hr/editjoboffers/:id', hrController.getEditJobOffers);
+router.post('/hr/editjoboffers/:jobId', hrController.updateJobOffer);
 router.get('/hr/records-performance-tracker', hrController.getRecordsPerformanceTracker);
 router.get('/hr/records-performance-tracker/:userId', hrController.getRecordsPerformanceTrackerByUserId);
 router.get('/hr/view-mrf/:id', hrController.getViewMRF);
