ff5eecc3549199c21c6c5ed945b67e1fb75cf8c6
Jushk <justinepicaze@gmail.com>
2024-12-07 14:39:20 +0800

    fixing

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = 4000;
 
 // Initialize Supabase client with your Supabase URL and API Key
 const supabaseUrl = 'https://amzzxgaqoygdgkienkwf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtenp4Z2Fxb3lnZGdraWVua3dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQzMjk0MzUsImV4cCI6MjAzOTkwNTQzNX0.1GdKI-d9CnJoLn0-T_VdM2Pd75PVHzAYOPIg_f7sgDQ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtenp4Z2Fxb3lnZGdraWVua3dmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNDMyOTQzNSwiZXhwIjoyMDM5OTA1NDM1fQ.fehmJFpcNvrfxOlOKMafogtxTP1ny-3Kc9LIbG9DYgY';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
